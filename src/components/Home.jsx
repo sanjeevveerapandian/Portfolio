c918ef2255b2fe4d1a35cbabdc61789ca06e2770
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-08-26 01:10:52 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,19 +7,19 @@ const Home = () => {
       className="relative h-[50vh] bg-background text-secondary dark:bg-gray-800 dark:text-white flex items-center justify-center overflow-hidden"
     >
       <img
-        src="/Portfolio/public/banner.jpg" // Include the base path here
+        src="/Portfolio/public/banner.jpg" 
         alt="Banner"
         className="absolute inset-0 w-full h-full object-cover z-0"
       />
       <div className="relative z-10 text-center flex flex-col items-center justify-center h-full">
         <div className="mb-16">
-          <h1 className="text-4xl">Hello, I am</h1>
+          <h1 className="text-4xl">Hello, I'm</h1>
           <h2 className="text-6xl font-bold mt-2">Sanjeev Veerapandian</h2>
           <p className="text-2xl mt-2">Full-Stack Developer</p>
         </div>
         <div className="absolute bottom-[1%] flex items-center justify-center w-full z-20">
           <img
-            src="/Portfolio/public/profilepic.jpg" // Include the base path here
+            src="/Portfolio/public/profilepic.jpg" 
             alt="Profile"
             className="w-42 h-40 rounded-full border-4 border-white shadow-lg"
           />
